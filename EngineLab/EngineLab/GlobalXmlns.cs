997eb05090e4943ba61909d62c9f9366363c3613
Xenny76 <dredford@student.neumont.edu>
2025-10-07 15:11:01 -0600

    Initial project push

---
+++ EngineLab/EngineLab/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "EngineLab")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "EngineLab.Pages")]
---
+++ EngineLab/EngineLab/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace EngineLab
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
